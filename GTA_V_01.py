b98b49fdc108a5b849733b2f1dc6e231a6f40f1e
kazuki-shin <kazukis2@illinois.edu>
2017-08-12 19:03:12 -0500

    Ep 3 Commit

---
+++ GTA_V_01.py
@@ -0,0 +1,43 @@
+import numpy as np
+from PIL import ImageGrab
+import cv2
+import time
+import pyautogui
+from directkeys import PressKey, W, A, S, D
+
+def roi(img, vertices):
+    mask =  np.zeros_like(img)
+    cv2.fillPoly(mask, vertices,255)
+    masked = cv2.bitwise_and(img, mask)
+    return masked
+
+def process_img(image):
+    original_image = image
+    # convert to gray
+    processed_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # edge detection
+    processed_img =  cv2.Canny(processed_img, threshold1 = 200, threshold2=300)
+    vertices = np.array([[10,500],[10,300],[300,200],[500,200],[800,300],[800,500]])
+    processed_img = roi(processed_img, [vertices])
+    return processed_img
+
+def main():
+
+    last_time = time.time()
+    while True:
+        screen =  np.array(ImageGrab.grab(bbox=(0,40,800,640)))
+        #print('Frame took {} seconds'.format(time.time()-last_time))
+        last_time = time.time()
+        new_screen = process_img(screen)
+        cv2.imshow('window', new_screen)
+        #cv2.imshow('window',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
+
+main()
+   
+   
+   
+   
+   
